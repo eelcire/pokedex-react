54ce020bb3edeb25a75d5106854fa78cd6720723
eelcire <eelcire.eelcire@hotmail.com>
2019-10-01 21:22:16 -0500

    1.0.1

---
+++ src/components/layout/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const NavBar = () => {
+    return (
+        <div>
+            <nav className = "navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+                <a>Pokedex</a>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar;

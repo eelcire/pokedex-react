847bd4bf638bd8017b218573fe251988669c2510
eelcire <eelcire.eelcire@hotmail.com>
2019-10-03 00:27:26 -0500

    1.0.2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavBar from './components/layout/NavBar';
+import Dashboard from './components/layout/Dashboard'
 
 function App() {
   return (
-    <div>
+    <div className = "App">
       <NavBar />
+      <div className = "container">
+        <Dashboard />
+      </div>
     </div>
   );
 }

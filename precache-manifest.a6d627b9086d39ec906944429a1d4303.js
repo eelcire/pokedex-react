8993ad828429ed95ef1e7e66dce336a9467f0f26
eelcire <eelcire.eelcire@hotmail.com>
2019-10-04 00:38:30 -0500

    Updates

---
+++ precache-manifest.a6d627b9086d39ec906944429a1d4303.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7111dcfe613fda9f4167acc90637e409",
+    "url": "/pokedex-react/index.html"
+  },
+  {
+    "revision": "6b0d6efff6b5c8c0bfb3",
+    "url": "/pokedex-react/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "88e44d026bac1b8120f9",
+    "url": "/pokedex-react/static/css/main.772f06ad.chunk.css"
+  },
+  {
+    "revision": "6b0d6efff6b5c8c0bfb3",
+    "url": "/pokedex-react/static/js/2.6a669902.chunk.js"
+  },
+  {
+    "revision": "88e44d026bac1b8120f9",
+    "url": "/pokedex-react/static/js/main.b1a7b62e.chunk.js"
+  },
+  {
+    "revision": "c2ab4a685476449db6ee",
+    "url": "/pokedex-react/static/js/runtime-main.4b035d5c.js"
+  },
+  {
+    "revision": "4f67d0f61e2b15851f7e441270947538",
+    "url": "/pokedex-react/static/media/giphy.4f67d0f6.gif"
+  },
+  {
+    "revision": "f8427d3339902f051105c107da7b4d8d",
+    "url": "/pokedex-react/static/media/pokeball.f8427d33.png"
+  }
+]);

4cc0335abf83e47ada7070ff6c44c225872193ef
eelcire <eelcire.eelcire@hotmail.com>
2019-10-04 01:00:11 -0500

    1.0.5

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+
+const StyledLink = styled(Link)`
+    text-decoration: none;
+    color: black;
+    &:focus, &:hover, &:visited, &:link, &:active {
+        text-decoration: none;
+    }
+`;
 
 export default class NavBar extends Component {
     render() {
         return (
             <div>
                 <nav className = "navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-                    <a
+                    <StyledLink
                         href = "/"
                         className = "navbar-brand col-sm-3 col-md-2 mr-0 align-items-center"
                     >Pokedex
-                    </a>
+                    </StyledLink>
                 </nav>
             </div>
         )

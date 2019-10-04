e8d86c3bff1bd1fa65376527abf108b7b05026ef
eelcire <eelcire.eelcire@hotmail.com>
2019-10-04 00:21:40 -0500

    1.0.3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavBar from './components/layout/NavBar';
-import Dashboard from './components/layout/Dashboard'
+import Dashboard from './components/layout/Dashboard';
+import Pokemon from './components/pokemon/Pokemon'
+
+import backgroundImage from './pokeball.png';
 
 function App() {
   return (
-    <div className = "App">
-      <NavBar />
-      <div className = "container">
-        <Dashboard />
+    <Router>
+      <div className = "App" style = {{background: `url(${backgroundImage})`}}>
+        <NavBar />
+        <div className = "container">
+          <Switch>
+            <Route exact path = "/" component = {Dashboard} />
+            <Route exact path = "/pokemon/:pokemonIndex" component = {Pokemon} />
+            <Dashboard />
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
+    
   );
 }
 
